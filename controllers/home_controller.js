3fad4965c17af0ce2d5e1cfbb7c881e6e1c26dff
Kashish Goyal <Kashishgoyal961@gmail.com>
2022-05-20 17:04:42 +0530

    async await and try catch

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,7 @@
 const Post = require('../models/post');
 const User = require('../models/user');
 
-module.exports.home = function (req, res) {
+module.exports.home = async (req, res) => {
     // // from browser sending
     // console.log(req.cookies);
     // // from codeial_development
@@ -19,25 +19,47 @@ module.exports.home = function (req, res) {
     // })
 
     // populate the user of each of posts
-    Post.find({})
-        .populate('user')
-        .populate({
-            path: 'comments',
-            populate: {
-                path: 'user'
-            }
-        })
-        .exec(function (err, posts) {
-            User.find({}, function (err, users) {
-                return res.render('home', {
-                    title: "Home",
-                    posts: posts,
-                    all_users: users
-                });
-            })
-            if (err) {
-                console.log(err, "error in finding posts");
-            }
-        })
+    // Post.find({})
+    //     .populate('user')
+    //     .populate({
+    //         path: 'comments',
+    //         populate: {
+    //             path: 'user'
+    //         }
+    //     })
+    //     .exec(function (err, posts) {
+    //         User.find({}, function (err, users) {
+    //             return res.render('home', {
+    //                 title: "Home",
+    //                 posts: posts,
+    //                 all_users: users
+    //             });
+    //         })
+    //         if (err) {
+    //             console.log(err, "error in finding posts");
+    //         }
+    //     })
+
+    try {
+        let posts = await Post.find({})
+            .populate('user')
+            .populate({
+                path: 'comments',
+                populate: {
+                    path: 'user'
+                }
+            });
+
+        let users = await User.find({});
+        return res.render('home', {
+            title: "Home",
+            posts: posts,
+            all_users: users
+        });
+    } catch (err) {
+        if (err) {
+            console.log(err, "error");
+        }
+    }
 }
 
